f0eea1cfd8d70f3b8126fb42cfd9a1b6d98317ec
usmon <usmonkarimov0726@gmail.com>
2024-11-01 17:18:22 +0500

    Working on routers

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kar1mov-u/RSS-agregator
 
 go 1.22.5
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello world")
+
+	godotenv.Load(".env")
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the enviroment")
+	}
+
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
